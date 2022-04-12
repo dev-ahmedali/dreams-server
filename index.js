ebc5d447142bb180233750a7081bbe33275c8a5a
dev-ahmedali <ahmedali69325@gmail.com>
2022-04-12 15:14:02 +0600

    added getPost message and fix env setup

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import bodyParser from "body-parser"
 import cors from "cors"
-import mongoose from 'mongoose';
-import 'dotenv/config';
+import mongoose from 'mongoose'
+import dotenv  from "dotenv"
+
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config()
 
 app.use('/posts', postRoutes);
 
@@ -16,10 +17,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l0sv8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://dreams-dev:321dreamsapp@cluster0.l0sv8.mongodb.net/Dreams?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
