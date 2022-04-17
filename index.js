62f4e34a54e0569199a8a4222e52e947979fc951
dev-ahmedali <ahmedali69325@gmail.com>
2022-04-17 16:32:33 +0600

    err solved not yet

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ import postRoutes from './routes/posts.js';
 const app = express();
 dotenv.config()
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://dreams-dev:321dreamsapp@cluster0.l0sv8.mongodb.net/Dreams?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
